9834f6861972c45aec81e637af225ec0ff2e026a
ShaherAshraf <shaherashraf77@gmail.com>
2024-06-16 17:51:55 +0300

    .

--- frontend/src/pages/MyAccount/index.tsx
+++ frontend/src/pages/MyAccount/index.tsx
@@ -55,8 +55,7 @@ const MyAccount = () => {
     },
   });
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [paymentIntents, setPaymentIntents] = useState<AxiosResponse>();
+  const [paymentIntents] = useState<AxiosResponse>();
   const [cancelLoading, setCancelLoading] = useState(false);
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
