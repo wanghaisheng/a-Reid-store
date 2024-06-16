bee395303c964ba9329ecb2dcfb4ea18fe191558
ShaherAshraf <shaherashraf77@gmail.com>
2024-06-16 14:03:50 +0300

    adjust product view thumbnails ids

--- frontend/src/pages/Products/_data.ts
+++ frontend/src/pages/Products/_data.ts
@@ -7,7 +7,7 @@ type thumbsType = {
 
 export const productThumbs: thumbsType[] = [
   {
-    id: '2',
+    id: '1',
     thumbs: [
       '/assets/products/product-01.jpg',
       '/assets/products/product-01-thumb-01.jpg',
@@ -15,7 +15,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '3',
+    id: '2',
     thumbs: [
       '/assets/products/product-02.jpg',
       '/assets/products/product-02-thumb-01.jpg',
@@ -23,7 +23,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '4',
+    id: '3',
     thumbs: [
       '/assets/products/product-03.jpg',
       '/assets/products/product-03-thumb-01.jpg',
@@ -31,7 +31,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '5',
+    id: '4',
     thumbs: [
       '/assets/products/product-04.jpg',
       '/assets/products/product-04-thumb-01.jpg',
@@ -39,7 +39,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '6',
+    id: '5',
     thumbs: [
       '/assets/products/product-05.jpg',
       '/assets/products/product-05-thumb-01.jpg',
@@ -47,7 +47,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '7',
+    id: '6',
     thumbs: [
       '/assets/products/product-06.jpg',
       '/assets/products/product-06-thumb-01.jpg',
@@ -55,7 +55,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '8',
+    id: '7',
     thumbs: [
       '/assets/products/product-07.jpg',
       '/assets/products/product-07-thumb-01.jpg',
@@ -63,7 +63,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '9',
+    id: '8',
     thumbs: [
       '/assets/products/product-08.jpg',
       '/assets/products/product-08-thumb-01.jpg',
@@ -71,7 +71,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '10',
+    id: '9',
     thumbs: [
       '/assets/products/product-09.jpg',
       '/assets/products/product-09-thumb-01.jpg',
@@ -79,7 +79,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '11',
+    id: '10',
     thumbs: [
       '/assets/products/product-10.jpg',
       '/assets/products/product-10-thumb-01.jpg',
@@ -87,7 +87,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '12',
+    id: '11',
     thumbs: [
       '/assets/products/product-11.jpg',
       '/assets/products/product-11-thumb-01.jpg',
@@ -95,7 +95,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '13',
+    id: '12',
     thumbs: [
       '/assets/products/product-12.jpg',
       '/assets/products/product-12-thumb-01.jpg',
@@ -103,7 +103,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '14',
+    id: '13',
     thumbs: [
       '/assets/products/product-13.jpg',
       '/assets/products/product-13-thumb-01.jpg',
@@ -111,7 +111,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '15',
+    id: '14',
     thumbs: [
       '/assets/products/product-14.jpg',
       '/assets/products/product-14-thumb-01.jpg',
@@ -119,7 +119,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '16',
+    id: '15',
     thumbs: [
       '/assets/products/product-15.jpg',
       '/assets/products/product-15-thumb-01.jpg',
@@ -127,7 +127,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '17',
+    id: '16',
     thumbs: [
       '/assets/products/product-16.jpg',
       '/assets/products/product-16-thumb-01.jpg',
@@ -135,7 +135,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '18',
+    id: '17',
     thumbs: [
       '/assets/products/product-17.jpg',
       '/assets/products/product-17-thumb-01.jpg',
@@ -143,7 +143,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '19',
+    id: '18',
     thumbs: [
       '/assets/products/product-18.jpg',
       '/assets/products/product-18-thumb-01.jpg',
@@ -151,7 +151,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '20',
+    id: '19',
     thumbs: [
       '/assets/products/product-19.jpg',
       '/assets/products/product-19-thumb-01.jpg',
@@ -159,7 +159,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '21',
+    id: '20',
     thumbs: [
       '/assets/products/product-20.jpg',
       '/assets/products/product-20-thumb-01.jpg',
@@ -167,7 +167,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '22',
+    id: '21',
     thumbs: [
       '/assets/products/product-21.jpg',
       '/assets/products/product-21-thumb-01.jpg',
@@ -175,7 +175,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '23',
+    id: '22',
     thumbs: [
       '/assets/products/product-22.jpg',
       '/assets/products/product-22-thumb-01.jpg',
@@ -183,7 +183,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '24',
+    id: '23',
     thumbs: [
       '/assets/products/product-23.jpg',
       '/assets/products/product-23-thumb-01.jpg',
@@ -191,7 +191,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '25',
+    id: '24',
     thumbs: [
       '/assets/products/product-24.jpg',
       '/assets/products/product-24-thumb-01.jpg',
@@ -199,7 +199,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '26',
+    id: '25',
     thumbs: [
       '/assets/products/product-25.jpg',
       '/assets/products/product-25-thumb-01.jpg',
@@ -207,7 +207,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '27',
+    id: '26',
     thumbs: [
       '/assets/products/product-26.jpg',
       '/assets/products/product-26-thumb-01.jpg',
@@ -215,7 +215,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '28',
+    id: '27',
     thumbs: [
       '/assets/products/product-27.jpg',
       '/assets/products/product-27-thumb-01.jpg',
@@ -223,7 +223,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '29',
+    id: '28',
     thumbs: [
       '/assets/products/product-28.jpg',
       '/assets/products/product-28-thumb-01.jpg',
@@ -231,7 +231,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '30',
+    id: '29',
     thumbs: [
       '/assets/products/product-29.jpg',
       '/assets/products/product-29-thumb-01.jpg',
@@ -239,7 +239,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '31',
+    id: '30',
     thumbs: [
       '/assets/products/product-30.jpg',
       '/assets/products/product-30-thumb-01.jpg',
@@ -247,7 +247,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '32',
+    id: '31',
     thumbs: [
       '/assets/products/product-31.jpg',
       '/assets/products/product-31-thumb-01.jpg',
@@ -255,7 +255,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '33',
+    id: '32',
     thumbs: [
       '/assets/products/product-32.jpg',
       '/assets/products/product-32-thumb-01.jpg',
@@ -263,7 +263,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '34',
+    id: '33',
     thumbs: [
       '/assets/products/product-33.jpg',
       '/assets/products/product-33-thumb-01.jpg',
@@ -271,7 +271,7 @@ export const productThumbs: thumbsType[] = [
     ],
   },
   {
-    id: '35',
+    id: '34',
     thumbs: [
       '/assets/products/product-34.jpg',
       '/assets/products/product-34-thumb-01.jpg',
